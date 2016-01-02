87e4cfd4f76a45273b71af0e622e0c1e58723dec
delostik <delostik@126.com>
2016-01-02 15:54:45 +0800

    readd fe_idx

--- DSSM_Train/DSMlib/Sample.cs
+++ DSSM_Train/DSMlib/Sample.cs
@@ -90,15 +90,18 @@ namespace DSMlib
         public int batchsize = ParameterSetting.BATCH_SIZE;
         CudaPieceInt word_Idx;
         CudaPieceInt sample_Idx;
+        CudaPieceInt fea_Idx;
         CudaPieceInt emo_Idx;
         CudaPieceInt seg_Margin;
 
-        public int[] Fea_Mem { get { return emo_Idx.MemPtr; } }
+        public int[] Fea_Mem { get { return fea_Idx.MemPtr; } }
+        public int[] Emo_Mem { get { return emo_Idx.MemPtr; } }
         public int[] Sample_Mem { get { return sample_Idx.MemPtr; } }
         public int[] Word_Idx_Mem { get { return word_Idx.MemPtr; } }
         public int[] Seg_Margin_Mem { get { return seg_Margin.MemPtr; } }
 
-        public IntPtr Fea_Idx { get { return emo_Idx.CudaPtr; } }
+        public IntPtr Fea_Idx { get { return fea_Idx.CudaPtr; } }
+        public IntPtr Emo_Idx { get { return emo_Idx.CudaPtr; } }
         public IntPtr Sample_Idx { get { return sample_Idx.CudaPtr; } }
         public IntPtr Word_Idx { get { return word_Idx.CudaPtr; } }
         public IntPtr Seg_Margin { get { return seg_Margin.CudaPtr; } }
@@ -106,6 +109,7 @@ namespace DSMlib
         public LabeledBatchSample_Input(int MAX_BATCH_SIZE, int MAXELEMENTS_PERBATCH)
         {
             sample_Idx = new CudaPieceInt(MAX_BATCH_SIZE, true, true);
+            fea_Idx = new CudaPieceInt(MAX_BATCH_SIZE, true, true);
             emo_Idx = new CudaPieceInt(MAX_BATCH_SIZE, true, true);
             word_Idx = new CudaPieceInt(MAXELEMENTS_PERBATCH, true, true);
             seg_Margin = new CudaPieceInt(MAXELEMENTS_PERBATCH, true, true);
@@ -127,8 +131,13 @@ namespace DSMlib
             {
                 Fea_Mem[i] = mreader.ReadInt32();
             }
+            for (int i = 0; i < batchsize; i++)
+            {
+                Emo_Mem[i] = mreader.ReadInt32();
+            }
             // update cudaDataPiece sizes
             sample_Idx.Size = batchsize;
+            fea_Idx.Size = batchsize;
             emo_Idx.Size = batchsize;
             word_Idx.Size = elementSize;
             seg_Margin.Size = elementSize;
@@ -148,6 +157,7 @@ namespace DSMlib
         public void Batch_In_GPU()
         {
             sample_Idx.CopyIntoCuda();
+            fea_Idx.CopyIntoCuda();
             emo_Idx.CopyIntoCuda();
             word_Idx.CopyIntoCuda();
             seg_Margin.CopyIntoCuda();
@@ -156,6 +166,7 @@ namespace DSMlib
         public void Dispose()
         {
             sample_Idx.Dispose();
+            fea_Idx.Dispose();
             emo_Idx.Dispose();
             word_Idx.Dispose();
             seg_Margin.Dispose();
